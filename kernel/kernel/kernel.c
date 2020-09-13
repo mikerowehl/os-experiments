6768fbe31e5f04e5f595379e9ff10a7b18baca6c
Mike Rowehl <mikerowehl@gmail.com>
2020-09-12 18:24:25 -0700

    Test reading from the keyboard port without interrupts.

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,7 +2,21 @@
 
 #include <kernel/tty.h>
 
+unsigned char inPortB (unsigned short _port) {
+    unsigned char rv;
+    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
 void kernel_main(void) {
+	unsigned char keyIn;
+	unsigned char charOut;
+
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
+	while (1) {
+		keyIn = inPortB(0x60);
+		charOut = (unsigned char)'a' + (keyIn - 0x1e);
+	  terminal_putchar(charOut);
+	}
 }
